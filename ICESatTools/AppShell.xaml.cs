717a6331e34aa8e15e162273afc0167f7f0c1db5
disszzin <disszzin@yandex.ru>
2025-09-28 14:48:02 +0300

    initial source

---
+++ ICESatTools/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ICESatTools;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ICESatTools/Pages/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ICESatTools.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
